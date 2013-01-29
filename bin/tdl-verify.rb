7297cbce54f18ebf73b6ca6e440a64845716d8a5
Mo Morsi <mmorsi@redhat.com>
2013-01-29 12:16:46 -0500

    flush out basic tdl-utility, import tdl rng

--- bin/tdl-verify.rb
+++ bin/tdl-verify.rb
@@ -11,10 +11,23 @@
 require 'thor'
 require 'nokogiri'
 
+RELAX_FILE = 'data/tdl.rng'
+
 class TDLVerify < Thor
   desc "verify", "verify the tdl specified on the command line"
   def verify(path)
-    puts "TODO verify #{path}"
+    say "Validating TDL"
+    xsd = Nokogiri::XML::RelaxNG(File.read(RELAX_FILE))
+    doc = Nokogiri::XML(File.open(path))
+    errs = xsd.validate(doc)
+    if errs.empty?
+      say "TDL is valid"
+    else
+      say "Errors in TDL"
+      errs.each { |err|
+        say err.message
+      }
+    end
   end
 end
 
