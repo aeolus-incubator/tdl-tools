4c07f6aa0d4bac6c9509b8c0875ac69d5d97293c
Mo Morsi <mmorsi@redhat.com>
2013-01-30 13:23:03 -0500

    clean up tdl-verify utility

--- bin/tdl-verify.rb
+++ bin/tdl-verify.rb
@@ -10,24 +10,38 @@
 
 require 'thor'
 require 'nokogiri'
+require 'colored'
 
+VERSION = "0.1.0"
 RELAX_FILE = 'data/tdl.rng'
 
 class TDLVerify < Thor
-  desc "verify", "verify the tdl specified on the command line"
+  desc "verify [path-to-tdl]", "Verify the tdl specified on the command line"
   def verify(path)
-    say "Validating TDL"
+    say "Validating TDL".bold
     xsd = Nokogiri::XML::RelaxNG(File.read(RELAX_FILE))
     doc = Nokogiri::XML(File.open(path))
     errs = xsd.validate(doc)
     if errs.empty?
-      say "TDL is valid"
+      say " TDL is valid".bold.green
     else
-      say "Errors in TDL"
+      say " Errors in TDL".bold.red
       errs.each { |err|
-        say err.message
+        say "  #{err.message}".red
       }
     end
+  end
+
+  map "--version" => :__version
+  desc "--version", "Version information"
+  def __version
+    say "tdl-tools verify #{VERSION}"
+  end
+
+  desc "help", "Command usage and other information"
+  def help
+    say 'Verify a TDL'
+    super
   end
 end
 
