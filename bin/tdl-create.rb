cd1c536ac6618aa7b6a4c7d7fcfec0a840f9d02f
Mo Morsi <mmorsi@redhat.com>
2013-02-25 12:48:10 -0500

    add etdl support to tdl-create, support to query package api for packages

--- bin/tdl-create.rb
+++ bin/tdl-create.rb
@@ -11,6 +11,9 @@
 require 'thor'
 require 'nokogiri'
 
+# use fedora api to verify packages
+require 'pkgwat'
+
 VERSION = "0.1.0"
 SAMPLE_TDL  = 'data/sample.tdl'
 SAMPLE_ETDL = 'data/sample.etdl'
@@ -18,17 +21,19 @@ SAMPLE_ETDL = 'data/sample.etdl'
 class TDLCreate < Thor
   desc "tdl", "create a new TDL"
   method_options :interactive => :boolean
+  method_options :verify      => :boolean
   def tdl
     output = File.read(SAMPLE_TDL)
-    output = run_interactive(output) if options[:interactive]
+    output = run_interactive(output, options) if options[:interactive]
     puts output
   end
 
   desc "etdl", "create a new eTDL"
   method_options :interactive => :boolean
+  method_options :verify      => :boolean
   def etdl
     output = File.read(SAMPLE_ETDL)
-    output = run_interactive(output) if options[:interactive]
+    output = run_interactive(output, options) if options[:interactive]
     puts output
   end
 
@@ -45,15 +50,22 @@ class TDLCreate < Thor
   end
 
   private
-  def run_interactive(output)
+  def run_interactive(output, options = {})
     xml = Nokogiri::XML(output)
     say "CTRL-D at any point to terminate prompts and output (e)tdl"
 
     begin
+      packages   = []
       name        = ask("Template Name:")
       description = ask("Template Description:")
       os_name    = ask("OS Name:")
       os_version = ask("OS Version:")
+      while true
+        package = ask('Package:')
+        packages << package
+      end
+      # TODO also files and commands
+      # TODO also cloud and verify for etdls
     rescue Exception => e
       say "\n"
     end
@@ -62,6 +74,23 @@ class TDLCreate < Thor
     xml.xpath('/template/description').first.content = description
     xml.xpath('/template/os/name').first.content = os_name
     xml.xpath('/template/os/version').first.content = os_version
+    pkgnode = xml.xpath('/template/packages').first
+    pkgnode.children[0..1].remove
+    packages.each { |pkg|
+      # verify
+      if options[:verify]
+        fpkg = Pkgwat.get_package(pkg)
+        raise "Could not find package #{pkg} in Fedora" if fpkg.nil?
+      end
+
+      node = Nokogiri::XML::Node.new('package', xml)
+      node['name'] = pkg
+      pkgnode << node
+
+      node = pkgnode.children.first.clone
+      pkgnode << node
+    }
+
     output = xml.to_s
   end
 
