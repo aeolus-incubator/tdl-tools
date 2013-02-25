6b6889095404dc8214f89638db4ee005caa1da36
Mo Morsi <mmorsi@redhat.com>
2013-02-25 15:28:00 -0500

    more etdl processor in tdl-launch to its own module

--- bin/tdl-launch.rb
+++ bin/tdl-launch.rb
@@ -24,105 +24,7 @@ require 'nokogiri'
 require 'tempfile'
 
 require 'etdl'
-require 'cloud_inst'
-
-
-###############################################################
-# Deploy various subsystems to do work
-
-class TDLLaunch
-  def launch_instance(etdl)
-    puts "Connecting to deltacloud instance at #{TDLTools::CloudInst::DC_URL}...".green
-    inst = TDLTools::CloudInst.new etdl.cloud_attributes
-
-    puts "Booting up cloud instance".green
-    puts "  This will take a few minutes to become fully accessible".green
-    inst.launch
-
-    if inst.address.nil?
-      puts "could not find address to access instance with".red
-      exit 1
-    end
-
-    puts "using address #{inst.address}\nssh:#{inst.ssh}\nscp:#{inst.scp}".green
-
-    inst
-  end
-
-  def process(etdl, instance)
-    # TODO need to open firewall ports (we have to open them manually now)
-
-    # set hostname
-    hostname = etdl.instance_attributes[:hostname]
-      unless hostname.nil?
-        puts "Setting hostname to #{hostname}".green
-        instance.exec "sudo hostname #{hostname}"
-
-        # append hostname to /etc/hosts/
-        etdl.instance_attributes[:files] <<
-          {:name => '/etc/hosts', :append => true,
-           :owner => 'root', :group => 'root', :mode => 644,
-           :contents => "#{instance.address} #{hostname}"}
-      end
-
-    # yum install packages
-    packages = etdl.instance_attributes[:packages].join(" ")
-      puts "installing #{packages}".green
-      puts instance.exec("sudo yum install -y --nogpgcheck #{packages}").blue
-
-    # start services
-    services = etdl.instance_attributes[:services]
-      services.each { |s|
-        puts "starting/enabling service #{s[:name]}".green
-        s[:pre].each { |cmd|
-          puts " running precommand #{cmd}".green
-          puts instance.exec("sudo #{cmd}").blue
-        }
-        puts instance.exec("sudo service #{s[:name]} start").blue
-        puts instance.exec("sudo chkconfig --levels 35 #{s[:name]} on").blue
-        s[:post].each { |cmd|
-          puts " running postcommand #{cmd}".green
-          puts instance.exec("sudo #{cmd}").blue
-        }
-      }
-
-    # create dirs
-    dirs = etdl.instance_attributes[:dirs]
-      dirs.each { |d|
-        puts "creating dir #{d[:name]}".green
-        instance.exec("sudo rm -rf #{d[:name]}") if d[:remove]
-        instance.exec("sudo mkdir -p #{d[:name]}")
-        instance.exec("sudo chown #{d[:owner]}.#{d[:group]} #{d[:name]}")
-      }
-
-    # copy files over
-    files = etdl.instance_attributes[:files]
-      files.each { |f|
-        tf = Tempfile.new('tdl-launch')
-        tf.write f[:contents]
-        tf.close
-
-        instance.cp tf.path, f[:name]
-        instance.exec("sudo chown #{f[:owner]}.#{f[:group]} #{f[:name]}")
-        instance.exec("sudo chmod #{f[:mode]} #{f[:name]}")
-      }
-
-    cmds = etdl.instance_attributes[:commands]
-      cmds.each { |c|
-        puts "running command #{c[:cmd]} as #{c[:user]}"
-        puts instance.exec("sudo -u #{c[:user]} -i #{c[:cmd]}").blue
-      }
-  end
-
-  def verify(etdl, instance)
-    # TODO
-  end
-
-  def terminate_instance(instance)
-    # TODO
-  end
-end
-
+require 'etdl_processor'
 
 ###############################################################
 # Read in configuration from cmd line and xml file
@@ -142,7 +44,7 @@ end
 doc  = Nokogiri::XML(open(ARGV.first))
 etdl = TDLTools::ETDL.parse(doc)
 
-launch = TDLLaunch.new
+launch = ETDLProcessor.new
 
 etdl.process(launch)
 
