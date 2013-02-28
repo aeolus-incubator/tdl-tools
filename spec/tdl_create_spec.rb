571ed6858438bede5df3c3dd642a62ff7e7c1d05
Mo Morsi <mmorsi@redhat.com>
2013-02-28 15:48:37 -0500

    expand tdl-create specs to test etdl support

--- spec/tdl_create_spec.rb
+++ spec/tdl_create_spec.rb
@@ -1,27 +1,29 @@
 require 'nokogiri'
 
 describe "tdl-create" do
-  it "should create a new tdl" do
-    output = `./bin/tdl-create.rb tdl`
-    $?.exitstatus.should == 0
+  ['tdl', 'etdl'].each { |tdl|
+    it "should create a new #{tdl}" do
+      output = `./bin/tdl-create.rb #{tdl}`
+      $?.exitstatus.should == 0
 
-    xsd = Nokogiri::XML::RelaxNG(File.read('data/tdl.rng'))
-    doc = Nokogiri::XML(output)
-    xsd.validate(doc).should be_empty
-  end
+      xsd = Nokogiri::XML::RelaxNG(File.read("data/#{tdl}.rng"))
+      doc = Nokogiri::XML(output)
+      xsd.validate(doc).should be_empty
+    end
 
-  it "should take parameters to create a tdl with" do
-    output = `./bin/tdl-create.rb tdl --interactive < spec/data/interactive_input`
-    $?.exitstatus.should == 0
+    it "should take parameters to create a #{tdl} with" do
+      output = `./bin/tdl-create.rb #{tdl} --interactive < spec/data/interactive_input`
+      $?.exitstatus.should == 0
 
-    # XXX remove prompts from output
-    output.slice!(0..124)
+      # XXX remove prompts from output
+      output.slice!(0..124)
 
-    doc = Nokogiri::XML(output)
-    doc.xpath('/template/name').first.content.should == 'custom-name'
-    doc.xpath('/template/description').first.content.should == 'custom description'
+      doc = Nokogiri::XML(output)
+      doc.xpath('/template/name').first.content.should == 'custom-name'
+      doc.xpath('/template/description').first.content.should == 'custom description'
 
-    xsd = Nokogiri::XML::RelaxNG(File.read('data/tdl.rng'))
-    xsd.validate(doc).should be_empty
-  end
+      xsd = Nokogiri::XML::RelaxNG(File.read("data/#{tdl}.rng"))
+      xsd.validate(doc).should be_empty
+    end
+  }
 end
