1868cfc2aebee9dc540ee7cb42ed1dad3498cba0
Mo Morsi <mmorsi@redhat.com>
2013-02-28 15:44:28 -0500

    expand tdl-verify specs to test etdl support

--- spec/tdl_verify_spec.rb
+++ spec/tdl_verify_spec.rb
@@ -8,4 +8,14 @@ describe "tdl-verify" do
    `./bin/tdl-verify.rb verify spec/data/invalid.tdl`
    $?.exitstatus.should == 1
   end
+
+  it "should validate valid etdl" do
+   `./bin/tdl-verify.rb verify spec/data/valid.etdl`
+   $?.exitstatus.should == 0
+  end
+
+  it "should validate invalid etdl" do
+   `./bin/tdl-verify.rb verify spec/data/invalid.etdl`
+   $?.exitstatus.should == 1
+  end
 end
