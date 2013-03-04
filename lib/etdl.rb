c604711913c83356a2412b0c333904f3b0e91d63
Mo Morsi <mmorsi@redhat.com>
2013-03-04 11:34:49 -0500

    start implementing new etdl format in tdl-launch

--- lib/etdl.rb
+++ lib/etdl.rb
@@ -14,39 +14,50 @@ class ETDL
   attr_reader :cloud_attributes
 
   # instance attributes
-  #attr_reader :name, :description, :hostname, :firewall, :packages, :services, :files, :dirs, :commands
+  #attr_reader :name, :description, :os, :repositories, :disk, :packages, :files, :commands
   attr_reader :instance_attributes
+
+  # additional commands to verify instance
+  attr_reader :verify_cmds
 
   def initialize(args = {})
     @cloud_attributes    = args[:cloud_attributes]    || {}
     @instance_attributes = args[:instance_attributes] || {}
+    @verify_cmds         = args[:verify_cmds]         || []
   end
 
   # Parse / return new eTDL instance from xml document
   def self.parse(doc)
     cloud_attributes    = {}
     instance_attributes = {}
+    verify_cmds         = []
 
     doc.children.last.children.each { |c|
       if c.name == 'name'
-        instance_attributes[:name] = c.text
+        instance_attributes[:name] = c.text.strip
 
       elsif c.name == 'description'
-        instance_attributes[:description] = c.text
-
-      elsif c.name == 'hostname'
-        instance_attributes[:hostname] = c.text
+        instance_attributes[:description] = c.text.strip
 
       elsif c.name == 'cloud'
         c.children.each { |ca|
-          cloud_attributes[ca.name.intern] = ca.text
+          cloud_attributes[ca.name.intern] = ca.text.strip
         }
 
-      elsif c.name == 'firewall'
-        c.children.each { |rule|
-          if rule.name == "tcp" || rule.name == "udp"
-            instance_attributes[:firewall] ||= []
-            instance_attributes[:firewall] << {:proto => rule.name, :value => rule.text}
+      elsif c.name == 'os'
+        instance_attributes[:os] = {}
+        c.children.each { |cc|
+          instance_attributes[:os][cc.name] = cc.text.strip
+        }
+
+      # TODO c.name == 'disk'
+
+      elsif c.name == 'repositories'
+        c.children.each { |repo|
+          unless repo['name'].nil?
+            instance_attributes[:repositories] ||= []
+            instance_attributes[:repositories] << { :name => repo['name'],
+                                                    :url  => repo['url'] }
           end
         }
 
@@ -58,60 +69,39 @@ class ETDL
           end
         }
 
-      elsif c.name == 'services'
-        c.children.each { |srv|
-          unless srv['name'].nil?
-            pre_cmds = []
-            post_cmds = []
-            srv.children.each { |s|
-              if s.name == "before"
-                pre_cmds << s.text
-              elsif s.name == "after"
-                post_cmds << s.text
-              end
-            }
-            instance_attributes[:services] ||= []
-            instance_attributes[:services] << {:name => srv['name'], :pre => pre_cmds, :post => post_cmds}
-          end
-        }
-
-      elsif c.name == 'dirs'
-        c.children.each { |dir|
-          owner = dir['owner'] || 'root'
-          group = dir['group'] || 'root'
-          remove = dir['remove'] || false
-          instance_attributes[:dirs] ||= []
-          instance_attributes[:dirs] << {:name => dir.text, :owner => owner, :group => group, :remove => remove} if dir.text.strip != ""
-        }
-
       elsif c.name == 'files'
         c.children.each { |file|
           unless file['name'].nil?
-            mode = file['mode'] || '644'
-            owner = file['owner'] || 'root'
-            group = file['group'] || 'root'
             instance_attributes[:files] ||= []
-            instance_attributes[:files] << {:name => file['name'], :mode => mode, :append => file['append'],
-                                            :owner => owner, :group => group,
-                                            :contents => file.text}
+            instance_attributes[:files] << {:name => file['name'],
+                                            :contents => file.text.strip}
           end
         }
 
       elsif c.name == 'commands'
         c.children.each { |cmd|
           unless cmd.name != "command"
-            user = cmd['user'] || 'root'
             instance_attributes[:commands] ||= []
-            instance_attributes[:commands] << {:cmd => cmd.text, :user => user}
+            instance_attributes[:commands] << {:cmd => cmd.text.strip}
           end
         }
+
+      elsif c.name == 'verify'
+        c.children.each { |cmd|
+          unless cmd.name != "verify"
+            verify_cmds cmd.text.strip
+          end
+        }
+
       end
     }
     #pp instance_attributes
     #pp cloud_attributes
+    #pp verify_cmds
 
     etdl = ETDL.new :cloud_attributes    => cloud_attributes,
-                    :instance_attributes => instance_attributes
+                    :instance_attributes => instance_attributes,
+                    :verify_cmds => verify_cmds
   end
 
   def process(processor)
